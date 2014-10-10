f3ca41f5bf1533e3b7d4a7463c7fdfcf9ce9cc19
Trevor Livingston <trlivingston@paypal.com>
2014-10-10 13:26:52 -0500

    Clean up.

--- test/benchmark/search.js
+++ test/benchmark/search.js
@@ -1,20 +1,20 @@
 'use strict';
 
-var wangle = require('../../lib'),
+var paths = require('../../lib/paths'),
     hammer = require('hammertime');
 
-var router = wangle();
+var router = paths();
 
-router.add('/foo/{id}/baz', true);
-router.add('/foo/{id}/baz/ball', true);
-router.add('/foobar/{id}/baz', true);
-router.add('/foobar/{id}/baz', true);
-router.add('/hello/world', true);
-router.add('/hello/world/{id}', true);
-router.add('/goodbye/world', true);
-router.add('/hello/world/{id}', true);
+router.insert('/foo/{id}/baz', true);
+router.insert('/foo/{id}/baz/ball', true);
+router.insert('/foobar/{id}/baz', true);
+router.insert('/foobar/{id}/baz', true);
+router.insert('/hello/world', true);
+router.insert('/hello/world/{id}', true);
+router.insert('/goodbye/world', true);
+router.insert('/hello/world/{id}', true);
 
-console.log(router.match('/foo/bar/baz'));
+console.log(router.search('/foo/bar/baz'));
 
 hammer({
     iterations: 10000,
@@ -22,5 +22,5 @@ hammer({
         console.dir(results);
     }
 }).time(function () {
-    router.match('/foo/bar/baz');
+    router.search('/foo/bar/baz');
 });
